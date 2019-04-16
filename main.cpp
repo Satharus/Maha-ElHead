9ed7485661fda59d6cc13f30f2311a0928e60bba
Satharus <a.elmayyah@gmail.com>
2019-04-16 19:05:53 +0200

    Testing something with git, this should be the short desc.
    This should be the long description.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main()
 	cout << "Hello" << endl;
 	cout << "Taher was here, i don't need to add a new file to indicate my huge importance in this shitty community, peaceOut" << endl;
 	cout << "Adam here: shitty f 3enak :D" << endl;
-	
+	cout << "Testing something with Git" << endl;	
 	return 0;
 }
